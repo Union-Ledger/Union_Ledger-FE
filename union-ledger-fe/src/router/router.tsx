6a6378210a40dad51dd166a2a0d1a25a030a4461
hwanggyuun <gka365@naver.com>
2026-04-07 22:38:25 +0900

    [feat] restructure router to include AppLayout (not login)

--- union-ledger-fe/src/router/router.tsx
+++ union-ledger-fe/src/router/router.tsx
@@ -6,38 +6,44 @@ import Template from "@pages/template/Template";
 import Upload from "@pages/upload/Upload";
 import Compare from "@pages/compare/Compare";
 import Create from "@pages/create/Create";
+import AppLayout from "@shared/components/layout/AppLayout";
 
 const router = createBrowserRouter([
   {
     element: <Outlet />,
     children: [
       {
-        path: ROUTES.DASHBOARD,
-        element: <Dashboard />,
-      },
-      {
         path: ROUTES.LOGIN,
         element: <Login />,
       },
       {
-        path: ROUTES.TEMPLATE,
-        element: <Template />,
-      },
-      {
-        path: ROUTES.UPLOAD,
-        element: <Upload />,
-      },
-      {
-        path: ROUTES.COMPARE,
-        element: <Compare />,
-      },
-      {
-        path: ROUTES.CREATE,
-        element: <Create />,
-      },
-      {
-        path: "*",
-        element: <div>404 Not Found</div>,
+        element: <AppLayout />,
+        children: [
+          {
+            path: ROUTES.DASHBOARD,
+            element: <Dashboard />,
+          },
+          {
+            path: ROUTES.TEMPLATE,
+            element: <Template />,
+          },
+          {
+            path: ROUTES.UPLOAD,
+            element: <Upload />,
+          },
+          {
+            path: ROUTES.COMPARE,
+            element: <Compare />,
+          },
+          {
+            path: ROUTES.CREATE,
+            element: <Create />,
+          },
+          {
+            path: "*",
+            element: <div>404 Not Found</div>,
+          },
+        ],
       },
     ],
   },
