0bb08fe53631c6c21b4065dd10197f41e6815795
hwanggyuun <gka365@naver.com>
2026-04-07 15:19:43 +0900

    [feat] update App component to use RouterProvider with Suspense for loading state

--- union-ledger-fe/src/App.tsx
+++ union-ledger-fe/src/App.tsx
@@ -1,5 +1,13 @@
+import { Suspense } from "react";
+import { RouterProvider } from "react-router-dom";
+import router from "@router/router";
+
 const App = () => {
-  return <div>유니온 레저</div>;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <RouterProvider router={router} />
+    </Suspense>
+  );
 };
 
 export default App;
